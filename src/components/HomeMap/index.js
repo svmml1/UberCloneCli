27eaef29d9a44dbc6f4d8db23970a2eb0be3056c
Sergio Victor <sergiovmml@gmail.com>
2021-07-29 23:53:33 -0300

    1

---
+++ src/components/HomeMap/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+export function HomeMap() {
+    return (
+       <View style={{
+           height: 300, 
+           backgroundColor:"#aaa",
+           justifyContent: 'center',
+           alignItems: 'center'
+           }}>
+           <Text>Im a map</Text>
+       </View>
+    )
+}
---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,17 @@
+import React from 'react' 
+import { View, Text } from 'react-native'
+
+import {HomeMap} from '../../components/HomeMap'
+
+export function HomeScreen() {
+    return(
+        <View>
+            <HomeMap />
+
+            {/* Covid Message */}
+
+
+            {/* Bottom components */}
+        </View>
+    )
+}
